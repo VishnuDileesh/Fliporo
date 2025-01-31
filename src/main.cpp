ac9c618c5dce8a28e4b06faaa891fa16e0a47291
VishnuDileesh <munnadileesh@gmail.com>
2025-01-31 19:14:51 +0530

    Add - gyro data is displayed to serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int MPU_ADDR = 0x68;
+
+float GyroX, GyroY, GyroZ; 
+float gyroAngleX, gyroAngleY, gyroAngleZ;
+float roll, pitch, yaw;
+float GyroErrorX, GyroErrorY, GyroErrorZ;
+float elapsedTime, currentTime, previousTime;
+int c = 0;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(19200);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission(true);
+  delay(20); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // === Read Gyroscope data === //
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x43); // Gyro data first register address 0x43
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  
+  // For a 250deg/s range we need to divide first the raw value by 131.0, according to the datasheet
+  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+
+  // Print the values on Serial Monitor
+  Serial.print("Gyro: ");
+  Serial.print("X = "); Serial.print(GyroX);
+  Serial.print(" | Y = "); Serial.print(GyroY);
+  Serial.print(" | Z = "); Serial.println(GyroZ);
+  
+  delay(100);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
